c27ce3ff9919484c78437ea8bf1c1a1c6478cc81
Anurag19999 <anuragkothari37@gmail.com>
2021-09-23 10:54:45 -0700

    Main application

--- src/main/java/com/project1/Project1Server/Project1ServerApplication.java
+++ src/main/java/com/project1/Project1Server/Project1ServerApplication.java
@@ -2,7 +2,9 @@ package com.project1.Project1Server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class Project1ServerApplication {
 
